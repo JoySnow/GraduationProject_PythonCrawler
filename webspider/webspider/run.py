f5d3596f1832b3d5103e6a54de73f322c128e15b
XiaoxueWang <xxwjoy@hotmail.com>
2016-04-14 13:08:36 +0800

    V1.0 finish the ShanghaiRanking Part.

--- webspider/webspider/run.py
+++ webspider/webspider/run.py
@@ -1,23 +1,46 @@
 from spiders.shanghairanking import ShanghairankingSpider
+from spiders.dmoz_spider import DmozSpider
 
 from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+from scrapy.settings import Settings
 import json
+import xlwt
 
-#json_file = file("./config_shanghairanking.json")
 json_file = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_shanghairanking_v2.json")
 config_rules = json.load(json_file)
 
-process = CrawlerProcess(get_project_settings())
+# open Workbook
+workbook = xlwt.Workbook(encoding='utf-8', style_compression=0)
 
-# 'followall' is the name of one of the spiders of the project.
-#process.crawl('', domain='scrapinghub.com')
+settings = Settings()
+
+# crawl settings
+#settings.set("USER_AGENT", "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
+#settings.set("ITEM_PIPELINES" , {
+#    'pipelines.DuplicatesPipeline': 200,
+#    # 'pipelines.CountDropPipline': 100,
+#    'pipelines.DataBasePipeline': 300,
+#})
+
+process = CrawlerProcess(settings)
+
+print "process"
+print process
+print "type(process):"
+print type(process)
 
 
 for ranking_name in config_rules:
     print ranking_name
-    new_spider = ShanghairankingSpider(config_rules[ranking_name])
-    process.crawl(new_spider)
+    worksheet = workbook.add_sheet(ranking_name)
+    process.crawl(ShanghairankingSpider, config_rules[ranking_name], worksheet)
+    
 
+print "process.start() here ..."
 process.start() # the script will block here until the crawling is finished
+
+# save workboot
+print "before save workbook ..."
+workbook.save("ShanghaiRanking.xls")
+print "after save workbook ..."
 
