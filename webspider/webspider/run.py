db8d7015fbb3ef42d1e14a31d90a5ce51d2ed332
XiaoxueWang <xxwjoy@hotmail.com>
2016-05-05 16:31:27 +0800

    finish nseac, using cuaa spider adding usnewsranking.py (finished, with a little problem) updata status_0427.txt

--- webspider/webspider/run.py
+++ webspider/webspider/run.py
@@ -2,6 +2,7 @@ from spiders.shanghairanking import ShanghairankingSpider
 from spiders.qsranking import QSrankingSpider
 from spiders.webometricsranking import WOrankingSpider
 from spiders.cuaaranking import CUAArankingSpider
+from spiders.usnewsranking import USNEWSrankingSpider
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
@@ -13,10 +14,14 @@ import logging
 ISOTIMEFORMAT='%Y%m%d%H%M%S'
 
 #xls_filename = "WORanking_" + str(time.strftime(ISOTIMEFORMAT)) + ".xls"
-xls_filename = "CUAARanking_" + str(time.strftime(ISOTIMEFORMAT)) + ".xls"
+#xls_filename = "CUAARanking_" + str(time.strftime(ISOTIMEFORMAT)) + ".xls"
+#xls_filename = "NSEACRanking_" + str(time.strftime(ISOTIMEFORMAT)) + ".xls"
+xls_filename = "USNEWSRanking_" + str(time.strftime(ISOTIMEFORMAT)) + ".xls"
 
 #log_filename = "woranking_" + str(time.strftime(ISOTIMEFORMAT)) + ".log"
-log_filename = "cuaaranking_" + str(time.strftime(ISOTIMEFORMAT)) + ".log"
+#log_filename = "cuaacranking_" + str(time.strftime(ISOTIMEFORMAT)) + ".log"
+#log_filename = "nseacranking_" + str(time.strftime(ISOTIMEFORMAT)) + ".log"
+log_filename = "usnewsranking_" + str(time.strftime(ISOTIMEFORMAT)) + ".log"
 
 logging.basicConfig(level=logging.INFO, filename=log_filename)
 logging.info("=============================================================")
@@ -26,15 +31,23 @@ logging.info("=============================================================")
 #json_file_shanghai = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_shanghairanking_v2.json")
 #json_file_qs = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_QS_ARWU_v2.json")
 #json_file_webometrics = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_webometrics_v2.json")
-json_file_cuaa = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_cuaa_v2.json")
+#json_file_cuaa = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_cuaa_v2.json")
+#json_file_nseac = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_nseac_v2.json")
+json_file_usnews = file("/home/joy/Git/GraduationProject_PythonCrawler/webspider/webspider/spiders/config_usnews_v2.json")
+
 #config_rules_shanghai = json.load(json_file_shanghai)
 #config_rules_qs = json.load(json_file_qs)
 #config_rules_webometrics = json.load(json_file_webometrics)
-config_rules_cuaa = json.load(json_file_cuaa)
+#config_rules_cuaa = json.load(json_file_cuaa)
+#config_rules_nseac = json.load(json_file_nseac)
+config_rules_usnews = json.load(json_file_usnews)
+
 #json_file_shanghai.close()
 #json_file_qs.close()
 #json_file_webometrics.close()
-json_file_cuaa.close()
+#json_file_cuaa.close()
+#json_file_nseac.close()
+json_file_usnews.close()
 
 # open Workbook
 workbook = xlwt.Workbook(encoding='utf-8', style_compression=0)
@@ -53,13 +66,15 @@ process = CrawlerProcess(settings)
 
 logging.info("process: %s " % process)
 
-for ranking_name in config_rules_cuaa:
+for ranking_name in config_rules_usnews:
     print ranking_name
     worksheet = workbook.add_sheet(ranking_name)
     #process.crawl(ShanghairankingSpider, config_rules_shanghai[ranking_name], worksheet)
     #process.crawl(WOrankingSpider, config_rules_webometrics[ranking_name], worksheet, logging)
-    process.crawl(CUAArankingSpider, config_rules_cuaa[ranking_name], worksheet,
-            logging)
+    # cuaa & nseac should share the same spider named CUAArankingSpider
+    #process.crawl(CUAArankingSpider, config_rules_cuaa[ranking_name], worksheet, logging)
+    #process.crawl(CUAArankingSpider, config_rules_nseac[ranking_name], worksheet, logging)
+    process.crawl(USNEWSrankingSpider, config_rules_usnews[ranking_name], worksheet, logging)
 
 logging.info("process.start() here ...")
 process.start() # the script will block here until the crawling is finished
